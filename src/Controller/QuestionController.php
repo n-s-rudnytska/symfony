d395874c4340780a12276ff26a6e3e9f640ed88e
n-s-rudnytska <85729320+n-s-rudnytska@users.noreply.github.com>
2021-07-14 22:30:42 +0300

    Service Config & Non-Autowireable Arguments

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace App\Controller;
+
+use App\Service\MarkdownHelper;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
+
 class QuestionController extends AbstractController
 {
     /**
@@ -13,24 +17,25 @@ class QuestionController extends AbstractController
      */
     public function homepage(Environment $twigEnvironment)
     {
-    
+
         return $this->render('question/homepage.html.twig');
     }
+
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache, MarkdownHelper $markdownHelper)
     {
-        dump($cache);
+        dump($this->getParameter('cache_adapter'));
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
         $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
-            return $markdownParser->transformMarkdown($questionText);
-        });
+
+        $parsedQuestionText = $markdownHelper->parse($questionText);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'questionText' => $parsedQuestionText,
