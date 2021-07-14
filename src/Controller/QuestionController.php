e372ff3f9166c10b3d1e18806b2100f1bad4ab25
n-s-rudnytska <85729320+n-s-rudnytska@users.noreply.github.com>
2021-07-14 19:20:41 +0300

    Cache Service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,13 +1,11 @@
 <?php
-
 namespace App\Controller;
-
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
-
 class QuestionController extends AbstractController
 {
     /**
@@ -15,29 +13,23 @@ class QuestionController extends AbstractController
      */
     public function homepage(Environment $twigEnvironment)
     {
-        /*
-        // fun example of using the Twig service directly!
-        $html = $twigEnvironment->render('question/homepage.html.twig');
-
-        return new Response($html);
-        */
-
+    
         return $this->render('question/homepage.html.twig');
     }
-
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
-
+        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'questionText' => $parsedQuestionText,
